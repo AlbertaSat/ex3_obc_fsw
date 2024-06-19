72ad4b946f32a6c1a3682ab2dd1591250aa2d56d
rrasmuss07 <rrasmuss@ualberta.ca>
2024-06-19 11:38:21 -0600

    coms handler rough draft update

--- handlers/handler/src/main.rs
+++ handlers/handler/src/main.rs
@@ -1,6 +1,6 @@
 /* Written by Rowan Rasmusson
-    This program will look at the MsgType field of a message and either send it
-    to the scheduler or directly to the Command Dispatcher
+    This program will look to see if a Msg has s specific dest_id,opcode combination
+    and send it to the scheduler or directly to the Command Dispatcher accordingly.
 */
 
 use interfaces::{self, TcpInterface};
@@ -8,14 +8,13 @@ use std::sync::mpsc;
 
 fn main() {
     // This stream will be the message as a whole
-    let ip: String = "127.0.0.1".to_string();
-    let port: u16 = 8080;
-    let tcp_interface: TcpInterface = interfaces::TcpInterface::new(ip,port).unwrap();
+    // let ip: String = "127.0.0.1".to_string();
+    // let port: u16 = 8080;
+    // let tcp_interface: TcpInterface = interfaces::TcpInterface::new(ip,port).unwrap();
 
-    let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
+    // let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
 
-    // should this funciton be private?
-    interfaces::async_read(tcp_interface.clone(), coms_handler_tx);
+    // interfaces::async_read(tcp_interface.clone(), coms_handler_tx);
 
     if let Ok(msg) = coms_handler_rx.recv() {
         // let curr_msg = from_bytes(msg);
@@ -33,13 +32,6 @@ fn main() {
     // write the byte stream to a TCP socket
 }
 // In charge of writing the message as a whole to the correct destination
-fn assign_msg(message: Message, tcp: TcpInterface, rx: Receiver<Vec<u8>>) {
-    match message.MsgType {
-        MsgType::ScheduledMsg(s) => {
-            println!("Sent to scheduler!");
-        },
-        _ => {
-            println!("Sent to CmdDispatcher!");
-        },
-    };
+fn assign_msg(message: Msg) -> Result<(), &'static str> {
+    
 }
