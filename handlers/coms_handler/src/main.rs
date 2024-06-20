f279f9ee72c7e91133f6731488a127c3a0e8deb7
rrasmuss07 <rrasmuss@ualberta.ca>
2024-06-20 09:53:41 -0600

    Changed directory to coms_handler. Waiting on IPC usage.

--- handlers/coms_handler/src/main.rs
+++ handlers/coms_handler/src/main.rs
@@ -6,25 +6,19 @@
 use interfaces::{self, TcpInterface};
 use std::sync::mpsc;
 use message_structure::*;
-use common::{*, dest_ids::IRIS};
+use common::*;
 
 fn main() {
-    // This stream will be the message as a whole
-    // let ip: String = "127.0.0.1".to_string();
-    // let port: u16 = 8080;
-    // let tcp_interface: TcpInterface = interfaces::TcpInterface::new(ip,port).unwrap();
-
-    // let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
-
-    // interfaces::async_read(tcp_interface.clone(), coms_handler_tx);
+    start_interface();
+    let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
     let mut curr_msg = Msg::new(0,0,0,0,vec![]);
     if let Ok(buffer) = coms_handler_rx.recv() {
         curr_msg = deserialize_msg(buffer);
     } else {
-        // read again?
+        Err("Cannot read Message")
     }
 
-    assign_msg(curr_msg, tcp_interface.clone());
+    assign_msg(curr_msg, tcp_interface.clone()).unwrap();
 
 
     // Procedure:
@@ -33,7 +27,7 @@ fn main() {
     // Look at the opcode and destination combo and send to sched or dispatcher
     // write the byte stream to a TCP socket
 }
-// In charge of writing the message as a whole to the correct destination
+// In charge of passing along the message as a whole to the correct destination
 fn assign_msg(msg: Msg) -> Result<(), &'static str> {
     let msg_opcode = msg.header.op_code;
     let msg_dest = msg.header.dest_id;
@@ -51,4 +45,15 @@ fn assign_msg(msg: Msg) -> Result<(), &'static str> {
         stream.write(serialized_msg);
          */
     }
+}
+
+fn start_interface() -> Result<(), &'static str> {
+    // This stream will be the message as a whole
+    // let ip: String = "127.0.0.1".to_string();
+    // let port: u16 = 8080;
+    // let tcp_interface: TcpInterface = interfaces::TcpInterface::new(ip,port).unwrap();
+
+    // let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
+
+    // interfaces::async_read(tcp_interface.clone(), coms_handler_tx);
 }
