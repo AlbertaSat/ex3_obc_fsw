d377b349cb51e0f81e66c217d3e4afc21dc92eb3
rrasmuss07 <rrasmuss@ualberta.ca>
2024-06-24 10:52:23 -0600

    Cleaned up function return types.

--- handlers/coms_handler/src/main.rs
+++ handlers/coms_handler/src/main.rs
@@ -1,24 +1,24 @@
 /* Written by Rowan Rasmusson
-    This program will look to see if a Msg has s specific dest_id,opcode combination
+    This program will look to see if a Msg has a specific dest_id,opcode combination
     and send it to the scheduler or directly to the Command Dispatcher accordingly.
 */
 
 use interfaces::{self, TcpInterface};
 use std::sync::mpsc;
 use message_structure::*;
-use common::*;
+use common::dest_ids::*;
 
-fn main() {
-    start_interface();
+fn main() -> Result<(), &'static str> {
+    let tcp_interface = start_handler().unwrap();
     let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
     let mut curr_msg = Msg::new(0,0,0,0,vec![]);
     if let Ok(buffer) = coms_handler_rx.recv() {
-        curr_msg = deserialize_msg(buffer);
+        curr_msg = deserialize_msg(buffer).unwrap();
     } else {
-        Err("Cannot read Message")
+        return Err("Cannot read Message");
     }
 
-    assign_msg(curr_msg, tcp_interface.clone()).unwrap();
+    assign_msg(curr_msg, tcp_interface.clone())?;
 
 
     // Procedure:
@@ -26,9 +26,10 @@ fn main() {
     // Build a message struct from the bytes
     // Look at the opcode and destination combo and send to sched or dispatcher
     // write the byte stream to a TCP socket
+    Ok(())
 }
 // In charge of passing along the message as a whole to the correct destination
-fn assign_msg(msg: Msg) -> Result<(), &'static str> {
+fn assign_msg(msg: Msg, tcp_interface: TcpInterface) -> Result<(), &'static str> {
     let msg_opcode = msg.header.op_code;
     let msg_dest = msg.header.dest_id;
     if msg_dest == IRIS && msg_opcode == 0 { //opcde 0 means take image for IRIS
@@ -37,6 +38,7 @@ fn assign_msg(msg: Msg) -> Result<(), &'static str> {
         let serialized_msg = serialize_msg(msg);
         stream.write(serialized_msg);
          */
+        Ok(())
     } else {
         /*
         let port = MSG_DISPATCHER_PORT;
@@ -44,16 +46,18 @@ fn assign_msg(msg: Msg) -> Result<(), &'static str> {
         let serialized_msg = serialize_msg(msg);
         stream.write(serialized_msg);
          */
+        Ok(())
     }
 }
 
-fn start_interface() -> Result<(), &'static str> {
+fn start_handler() -> Result<TcpInterface, std::io::Error> {
     // This stream will be the message as a whole
-    // let ip: String = "127.0.0.1".to_string();
-    // let port: u16 = 8080;
-    // let tcp_interface: TcpInterface = interfaces::TcpInterface::new(ip,port).unwrap();
+    let ip: String = "127.0.0.1".to_string();
+    let port: u16 = 8080;
+    interfaces::TcpInterface::new_server(ip,port)
 
     // let (coms_handler_tx, coms_handler_rx) = mpsc::channel();
 
     // interfaces::async_read(tcp_interface.clone(), coms_handler_tx);
+
 }
